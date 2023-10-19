09abb12f164376225e01fec4acd9df21a78810db
Kxxle <64878567+Kxxle@users.noreply.github.com>
2023-10-19 09:20:01 +0200

    I have completed all the calculations.

--- src/PrimaryClass.java
+++ src/PrimaryClass.java
@@ -1,8 +1,29 @@
 import java.io.IOException;
-import java
+import java.util.Scanner;
 
 public class PrimaryClass {
     public static void main(String[] args)throws IOException {
+        Scanner Scanner = new Scanner(System.in);
+        float firstNumber;
+        float secondNumber;
+
+
+        System.out.printf("Please enter the first number:");
+        firstNumber = Scanner.nextInt();
+        System.out.printf("Please enter the second number:");
+        secondNumber = Scanner.nextInt();
+
+//        This is all the operations and calculation
+        float Sum = firstNumber + secondNumber;
+        float Product = firstNumber * secondNumber;
+        float Difference = firstNumber - secondNumber;
+        float Quotient = firstNumber / secondNumber;
+
+        System.out.println("/////RESULT/////");
+        System.out.printf("\nThis is the result Sum of the numbers %.2f ",Sum);
+        System.out.printf("\nThis is the result of the product of the numbers %.2f" , Product);
+        System.out.printf("\nThis is the result of the difference of the numbers %.2f", Difference);
+        System.out.printf("\nThis is the result of the division of the numbers %.2f ",  Quotient);
 
     }
 }
